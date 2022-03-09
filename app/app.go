4b7a4d2d308df04527f2bf5686b10f54ce91be34
Cody <cody.w.zimmerman@gmail.com>
2022-03-08 19:57:17 -0600

    Ye

--- app/app.go
+++ app/app.go
@@ -3,14 +3,19 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 
+	//mux := http.NewServeMux()
+	router := mux.NewRouter()
+
 	// Routes
-	http.HandleFunc("/greet", greet)
-	http.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet)
+	router.HandleFunc("/customers", getAllCustomers)
 
 	// Starts
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/Codoou/go-banking/app"
+
 func main() {
-	Start()
+	app.Start()
 }
